145ec637f76cd8d7b8190a9fc857890e6c01657a
= <=>
2024-02-10 18:01:05 +0000

    initialised a 'libary' project

--- server/Server.Api/Program.cs
+++ server/Server.Api/Program.cs
@@ -6,13 +6,14 @@ namespace Server.Api
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateSlimBuilder(args);
+            WebApplicationBuilder builder = WebApplication.CreateSlimBuilder(args);
+
 
             builder.Services.ConfigureHttpJsonOptions(options =>
             {
                 options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
             });
-
+            
             var app = builder.Build();
 
             var sampleTodos = new Todo[] {
